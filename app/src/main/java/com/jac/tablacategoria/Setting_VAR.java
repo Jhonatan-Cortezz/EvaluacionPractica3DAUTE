7941ad38e5477a0361a0fafb4fa9690a8996626e
Jhonatan-Cortezz <jhonatancortez708@gmail.com>
2020-10-28 16:19:19 -0600

    Funcionalidad para guardar registros en MySql agregada

---
+++ app/src/main/java/com/jac/tablacategoria/Setting_VAR.java
@@ -0,0 +1,6 @@
+package com.jac.tablacategoria;
+
+public class Setting_VAR {
+    public static final String URL_PRUEBA = "http://192.168.100.5/service2020/json1.php";
+    public static final String URL_GUARDAR_CATEGORIAS = "http://192.168.100.5/service2020/guardar_categorias.php";
+}
---
+++ app/src/main/java/com/jac/tablacategoria/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,34 @@
+package com.jac.tablacategoria.ui.notifications;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.jac.tablacategoria.R;
+
+public class NotificationsFragment extends Fragment {
+    private Button btnNew;
+    private Button cancel;
+
+    private NotificationsViewModel notificationsViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        notificationsViewModel =
+                ViewModelProviders.of(this).get(NotificationsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        return root;
+    }
+}
