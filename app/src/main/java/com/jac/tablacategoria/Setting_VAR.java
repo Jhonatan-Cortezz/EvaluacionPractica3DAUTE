c86ff2eb25e415e37c07faff407c9b454f48f428
Jhonatan-Cortezz <jhonatancortez708@gmail.com>
2020-10-29 21:02:39 -0600

    Consulta a la tabla categroia y mostrar los datos en listView agregado

--- app/src/main/java/com/jac/tablacategoria/Setting_VAR.java
+++ app/src/main/java/com/jac/tablacategoria/Setting_VAR.java
@@ -3,4 +3,5 @@ package com.jac.tablacategoria;
 public class Setting_VAR {
     public static final String URL_PRUEBA = "http://192.168.100.5/service2020/json1.php";
     public static final String URL_GUARDAR_CATEGORIAS = "http://192.168.100.5/service2020/guardar_categorias.php";
+    public static final String URL_CONSULTAR_CATEGORIAS = "http://192.168.100.5/service2020/searchAllCategoria.php";
 }
--- app/src/main/java/com/jac/tablacategoria/ui/home/HomeFragment.java
+++ app/src/main/java/com/jac/tablacategoria/ui/home/HomeFragment.java
@@ -11,12 +11,14 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.LinearLayoutCompat;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
@@ -27,15 +29,19 @@ import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.jac.tablacategoria.MySingleton;
 import com.jac.tablacategoria.R;
 import com.jac.tablacategoria.Setting_VAR;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 //this class is a Entity for Table categoria
@@ -45,7 +51,11 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     private Button cancel, btnSave;
     private Spinner spEstado;
     private EditText edtId, edtNombre;
+    private LinearLayoutCompat resultado;
     private HomeViewModel homeViewModel;
+    private ListView lst;
+    ArrayList<String> lista = null;
+    ArrayList<DtoCategoria> listaCategoria;
 
     String datoSelect = "";
 
@@ -55,16 +65,20 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final ConstraintLayout frameLayout1 = root.findViewById(R.id.constraintLayout);
-        final TextView frameLayout2 = root.findViewById(R.id.fm2);
+        final LinearLayoutCompat frameLayout2 = root.findViewById(R.id.fm2);
         btnNew = root.findViewById(R.id.btnNuevo);
         edtId = root.findViewById(R.id.edtCategoria);
+        resultado = root.findViewById(R.id.fm2);
         edtNombre = root.findViewById(R.id.edtNombreCategoria);
         cancel = root.findViewById(R.id.btnCancel);
         spEstado = root.findViewById(R.id.sp_estado);
+        lst = root.findViewById(R.id.lstCategoria);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
                 R.array.estadoCategorias, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
 
         spEstado.setAdapter(adapter);
 
@@ -104,6 +118,8 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
 
         btnSave = root.findViewById(R.id.btnGuardarCate);
         btnSave.setOnClickListener(this);
+
+        recibirAllCat();
 
         return root;
     }
@@ -177,4 +193,58 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         MySingleton.getInstance(context).addToRequestQueue(request);
     }
 
+    private void recibirAllCat(){
+        listaCategoria = new ArrayList<DtoCategoria>();
+        lista = new ArrayList<String>();
+        String urlConsultaCategoria = Setting_VAR.URL_CONSULTAR_CATEGORIAS;
+        StringRequest request = new StringRequest(Request.Method.POST, urlConsultaCategoria, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray respuestaJSOn = new JSONArray(response);
+                    int totalEnct = respuestaJSOn.length();
+
+                    DtoCategoria objCategorias = null;
+                    for (int i = 0; i < respuestaJSOn.length(); i++){
+                        JSONObject categoriaObj = respuestaJSOn.getJSONObject(i);
+                        int idC = categoriaObj.getInt("id_categoria");
+                        String name = categoriaObj.getString("nom_categoria");
+                        int stado = categoriaObj.getInt("estado_categoria");
+
+                        objCategorias = new DtoCategoria(idC, name, stado);
+
+                        listaCategoria.add(objCategorias);
+
+                        lista.add(listaCategoria.get(i).getIdCategoria() + " - " + listaCategoria.get(i).getNombre());
+
+                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, lista);
+                        lst.setAdapter(adapter);
+
+                        Log.i("Id Categoria:    ", String.valueOf(objCategorias.getIdCategoria()));
+                        Log.i("Nombre:    ", String.valueOf(objCategorias.getNombre()));
+
+                    }
+                    //resultado.setText("Datos: " + response.toString());
+                    //Toast.makeText(getContext(), "Id: " + idCategori + "\nNombre: " + nombreCat + "\nEstado: " + estadoCat, Toast.LENGTH_SHORT).show();
+
+                } catch (JSONException ex){
+                    String none = ex.toString();
+                    Log.i("NO consulta ***** ", none);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                String err = volleyError.toString();
+                Log.i("No se pudo **********", err);
+            }
+        });
+        //tiempo de respuesta, establece politica de reintentos
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                10000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+        MySingleton.getInstance(getContext()).addToRequestQueue(request);
+    }
 }
