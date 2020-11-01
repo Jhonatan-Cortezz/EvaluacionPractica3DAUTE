8de3779ab85143c6e8f23f7a8b8320c7b7460496
Jhonatan-Cortezz <jhonatancortez708@gmail.com>
2020-10-31 19:37:55 -0600

    Proyecto funcionando en la nube, la conexiona a la base de datos es remota por medio de una hosting

--- app/src/main/java/com/jac/tablacategoria/Setting_VAR.java
+++ app/src/main/java/com/jac/tablacategoria/Setting_VAR.java
@@ -2,9 +2,9 @@ package com.jac.tablacategoria;
 
 public class Setting_VAR {
     public static final String URL_PRUEBA = "http://192.168.100.5/service2020/json1.php";
-    public static final String URL_GUARDAR_CATEGORIAS = "http://192.168.100.5/service2020/guardar_categorias.php";
-    public static final String URL_CONSULTAR_CATEGORIAS = "http://192.168.100.5/service2020/searchAllCategoria.php";
-    public static final String URL_DELETE_CATEGORIA = "http://192.168.100.5/service2020/eliminarCategoria.php";
-    public static final String URL_UPDATE_CATEGORIA = "http://192.168.100.5/service2020/actualizar.php";
-    public static final String URL_LOGIN_USUARIO = "http://192.168.100.5/service2020/login.php";
+    public static final String URL_GUARDAR_CATEGORIAS = "https://appiandroid.000webhostapp.com/guardar_categorias.php";
+    public static final String URL_CONSULTAR_CATEGORIAS = "https://appiandroid.000webhostapp.com/searchAllCategoria.php";
+    public static final String URL_DELETE_CATEGORIA = "https://appiandroid.000webhostapp.com/eliminarCategoria.php";
+    public static final String URL_UPDATE_CATEGORIA = "https://appiandroid.000webhostapp.com/actualizar.php";
+    public static final String URL_LOGIN_USUARIO = "https://appiandroid.000webhostapp.com/login.php";
 }
